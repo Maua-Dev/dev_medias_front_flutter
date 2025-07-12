623f4e0c7ffe1f3f4c04eb258d728861be67447f
tokujiTO <tiagomassuda123@gmail.com>
2025-07-12 16:30:44 -0300

    feat: delete logo of home and name on logging in

--- lib/app/widgets/starting_data_form.dart
+++ lib/app/widgets/starting_data_form.dart
@@ -15,7 +15,6 @@ class StartingDataForm extends StatefulWidget {
 }
 
 class _StartingDataFormState extends State<StartingDataForm> {
-  TextEditingController nameController = TextEditingController();
   TextEditingController graduationController = TextEditingController();
   TextEditingController yearController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -36,6 +35,7 @@ class _StartingDataFormState extends State<StartingDataForm> {
             ),
             Expanded(child: Container()),
             const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Text(
@@ -45,33 +45,18 @@ class _StartingDataFormState extends State<StartingDataForm> {
                     fontSize: 20,
                   ),
                 ),
-                Text(
-                  "Fale um pouco de você para começarmos",
-                  style: TextStyle(color: AppColors.white, fontSize: 16),
+                Center(
+                  child: Text(
+                    "Selecione seu curso e ano para começarmos",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.white,
+                      fontSize: 16,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
                 ),
               ],
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 18),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Seu nome",
-                    style: labelStyle(),
-                  ),
-                  TextFormField(
-                      controller: nameController,
-                      style: const TextStyle(fontSize: 20),
-                      decoration: customInputDecoration(hintText: ""),
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      }),
-                ],
-              ),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 18),
@@ -83,32 +68,32 @@ class _StartingDataFormState extends State<StartingDataForm> {
                     style: labelStyle(),
                   ),
                   DropdownMenu(
-                      width: 380,
-                      enableFilter: true,
-                      controller: graduationController,
-                      inputDecorationTheme: customInputDecorationTheme(),
-                      textStyle:
-                          const TextStyle(fontSize: 20, color: AppColors.black),
-                      onSelected: (s) {
-                        userController.setGraduation(s!);
-                      },
-                      menuHeight: 200,
-                      menuStyle: const MenuStyle(
-                        maximumSize:
-                            WidgetStatePropertyAll(Size.fromWidth(120)),
-                        backgroundColor:
-                            WidgetStatePropertyAll(AppColors.white),
-                      ),
-                      dropdownMenuEntries:
-                          graduationsController.getDropdownInputStrings().map((s) {
-                        return DropdownMenuEntry<String>(
-                          style: MenuItemButton.styleFrom(
-                              textStyle: const TextStyle(fontSize: 20),
-                              backgroundColor: AppColors.white),
-                          value: s,
-                          label: '$s - ${graduationsController.allGrads?[s]}',
-                        );
-                      }).toList()),
+                    width: 380,
+                    enableFilter: true,
+                    controller: graduationController,
+                    inputDecorationTheme: customInputDecorationTheme(),
+                    textStyle:
+                        const TextStyle(fontSize: 20, color: AppColors.black),
+                    onSelected: (s) {
+                      userController.setGraduation(s!);
+                    },
+                    menuHeight: 200,
+                    menuStyle: const MenuStyle(
+                      maximumSize: WidgetStatePropertyAll(Size.fromWidth(120)),
+                      backgroundColor: WidgetStatePropertyAll(AppColors.white),
+                    ),
+                    dropdownMenuEntries: graduationsController
+                        .getDropdownInputStrings()
+                        .map((s) {
+                      return DropdownMenuEntry<String>(
+                        style: MenuItemButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 20),
+                            backgroundColor: AppColors.white),
+                        value: s,
+                        label: '$s - ${graduationsController.allGrads?[s]}',
+                      );
+                    }).toList(),
+                  ),
                 ],
               ),
             ),
@@ -122,60 +107,62 @@ class _StartingDataFormState extends State<StartingDataForm> {
                     style: labelStyle(),
                   ),
                   DropdownMenu(
-                      width: 380,
-                      controller: yearController,
-                      enableFilter: true,
-                      inputDecorationTheme: customInputDecorationTheme(),
-                      textStyle:
-                          const TextStyle(fontSize: 20, color: AppColors.black),
-                      onSelected: (s) {
-                        userController.setYear(int.parse(s!));
-                      },
-                      dropdownMenuEntries: [1, 2, 3, 4, 5].map((s) {
-                        return DropdownMenuEntry<String>(
-                          style: MenuItemButton.styleFrom(
-                              textStyle: const TextStyle(
-                                  fontSize: 20,
-                                  overflow: TextOverflow.ellipsis),
-                              backgroundColor: AppColors.white),
-                          value: "$s",
-                          label: '$s',
-                        );
-                      }).toList()),
+                    width: 380,
+                    controller: yearController,
+                    enableFilter: true,
+                    inputDecorationTheme: customInputDecorationTheme(),
+                    textStyle:
+                        const TextStyle(fontSize: 20, color: AppColors.black),
+                    onSelected: (s) {
+                      userController.setYear(int.parse(s!));
+                    },
+                    dropdownMenuEntries: [1, 2, 3, 4, 5].map((s) {
+                      return DropdownMenuEntry<String>(
+                        style: MenuItemButton.styleFrom(
+                            textStyle: const TextStyle(
+                                fontSize: 20, overflow: TextOverflow.ellipsis),
+                            backgroundColor: AppColors.white),
+                        value: "$s",
+                        label: '$s',
+                      );
+                    }).toList(),
+                  ),
                 ],
               ),
             ),
             Expanded(flex: 1, child: Container()),
             ElevatedButton(
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    await userController.insertUserData(UserModel(
-                        name: nameController.text,
-                        graduation: graduationController.text,
-                        year: int.parse(yearController.text)));
-                    await userController.loadInitialCourses(graduationController.text, int.parse(yearController.text));
-                    introPageController.setLoginSuccesful(true);
-                    Navigator.pushReplacementNamed(context, '/home');
-                  }
-                },
-                style: TextButton.styleFrom(
-                    backgroundColor: AppColors.red,
-                    shape: RoundedRectangleBorder(borderRadius: Round.primary),
-                    minimumSize: const Size.fromHeight(50),
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 7, horizontal: 7)),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 12),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Confirmar",
-                        style: TextStyle(color: AppColors.white, fontSize: 22),
-                      ),
-                    ],
-                  ),
-                )),
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  await userController.insertUserData(UserModel(
+                      graduation: graduationController.text,
+                      year: int.parse(yearController.text)));
+                  await userController.loadInitialCourses(
+                      graduationController.text,
+                      int.parse(yearController.text));
+                  introPageController.setLoginSuccesful(true);
+                  Navigator.pushReplacementNamed(context, '/home');
+                }
+              },
+              style: TextButton.styleFrom(
+                backgroundColor: AppColors.red,
+                shape: RoundedRectangleBorder(borderRadius: Round.primary),
+                minimumSize: const Size.fromHeight(50),
+                padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 7),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Confirmar",
+                      style: TextStyle(color: AppColors.white, fontSize: 22),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Expanded(flex: 2, child: Container())
           ],
         ),
@@ -186,27 +173,19 @@ class _StartingDataFormState extends State<StartingDataForm> {
   TextStyle labelStyle() =>
       const TextStyle(color: AppColors.white, fontSize: 18.0);
 
-  InputDecoration customInputDecoration({String hintText = ""}) {
-    return InputDecoration(
-        hintText: hintText,
-        hintStyle: const TextStyle(fontSize: 20, color: AppColors.textFaded),
-        fillColor: AppColors.white,
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: Round.primary,
-          borderSide: BorderSide.none,
-        ));
-  }
-
   InputDecorationTheme customInputDecorationTheme({String hintText = ""}) {
     return InputDecorationTheme(
-        constraints: BoxConstraints.expand(),
-        hintStyle: const TextStyle(fontSize: 20, color: AppColors.textFaded),
-        fillColor: AppColors.white,
-        filled: true,
-        border: OutlineInputBorder(
-          borderRadius: Round.primary,
-          borderSide: BorderSide.none,
-        ));
+      constraints: const BoxConstraints(
+        minHeight: 50,
+        maxHeight: 60,
+      ),
+      hintStyle: const TextStyle(fontSize: 20, color: AppColors.textFaded),
+      fillColor: AppColors.white,
+      filled: true,
+      border: OutlineInputBorder(
+        borderRadius: Round.primary,
+        borderSide: BorderSide.none,
+      ),
+    );
   }
 }

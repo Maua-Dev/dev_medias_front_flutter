757f5ef5bd3245add2bdae8d1aa6a0fe3dffb40d
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-08-23 23:03:07 -0300

    try catch solution for out of pattern courses

--- lib/app/service/course_service.dart
+++ lib/app/service/course_service.dart
@@ -7,30 +7,44 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class CourseService {
   final Dio dio = Dio();
 
-  Future<Map<String, dynamic>> getCourses() async {
+  Future<Map<String, CourseModel>> getCourses() async {
     try {
       final response = await dio.get(dotenv.env['API_SUBJECTS']!);
+
       if (response.statusCode == 200) {
         Map<String, dynamic> data = response.data;
         Map<String, CourseModel> aux = {};
 
         data.forEach((code, course) {
-          // converte as provas no formato GradeModel
-          final prevExams = course["exams"];
-          final newExamList = [];
-          for (Map<String, dynamic> exam in prevExams) {
-            newExamList.add(GradeModel.fromJson(exam));
+          // Adicionado bloco try-catch para isolar erros de parsing
+          try {
+            // A sua lógica original é mantida aqui dentro do 'try'
+            final prevExams = course["exams"];
+            final newExamList = [];
+            for (Map<String, dynamic> exam in prevExams) {
+              newExamList.add(GradeModel.fromJson(exam));
+            }
+            course["exams"] = newExamList;
+
+            final prevAssignments = course["assignments"];
+            final newAssignmentList = [];
+            for (Map<String, dynamic> assignment in prevAssignments) {
+              newAssignmentList.add(GradeModel.fromJson(assignment));
+            }
+            course["assignments"] = newAssignmentList;
+
+            // Esta é a linha que provavelmente causa o erro se os tipos não baterem
+            var courseModel = CourseModel.fromJson(course);
+            aux[code] = courseModel;
+
+          } catch (error) {
+            // Se qualquer parte da lógica acima falhar para um curso específico,
+            // o erro será capturado aqui, e o loop continuará para o próximo.
+            print('--- ERRO DE PARSING EM UM CURSO ---');
+            print('Falha ao processar o curso com o código: $code');
+            print('Erro específico: $error');
+            print('------------------------------------');
           }
-          course["exams"] = newExamList;
-          // converte os trabalhos no formato GradeModel
-          final prevAssignments = course["assignments"];
-          final newAssignmentList = [];
-          for (Map<String, dynamic> assignment in prevAssignments) {
-            newAssignmentList.add(GradeModel.fromJson(assignment));
-          }
-          course["assignments"] = newAssignmentList;
-          course = CourseModel.fromJson(course);
-          aux[code] = course;
         });
 
         Map<String, CourseModel> courses = aux;
